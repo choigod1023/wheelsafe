00c016f77b01f0760793bff75f5096184375bacb
Jang junhyeok <choigod10234@gmail.com>
2023-07-08 17:50:43 +0900

    map backend connect

--- test/test.py
+++ test/test.py
@@ -1,17 +1,25 @@
-import requests
-from PIL import Image
-import binascii
-import io
-import base64
-url = "https://apis.openapi.sk.com/tmap/staticMap?version=1&coordType=WGS84GEO&width=512&height=512&zoom=15&format=JPG&longitude=126.83529138565&latitude=37.5446283608815&markers=126.978155%2C37.566371_126.108155%2C37.566371"
+import pymysql
+from flask import Flask, jsonify
+from flask_cors import CORS
+import json
 
-headers = {
-    "accept": "application/json",
-    "appKey": "e8wHh2tya84M88aReEpXCa5XTQf3xgo01aZG39k5"
-}
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+db = pymysql.connect(
+    user='admin',
+    password='jjang486',
+    host='database-1-instance-1.comumtztjsvx.ap-northeast-2.rds.amazonaws.com',
+    db='wheelsafe',
+)
+cursor = db.cursor(pymysql.cursors.DictCursor)
 
-response = requests.get(url, headers=headers,stream=True)
-if response.status_code == 200:
-    with open('test.jpg','wb') as f:
-        for chunk in response.iter_content(1024):
-                f.write(chunk)
+@app.route('/marker')
+def show_marker():
+    cursor.execute("""
+select * FROM marker
+""")
+    result = cursor.fetchall()
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)
